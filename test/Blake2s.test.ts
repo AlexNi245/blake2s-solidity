f57247068c429186c9387b43761b80dadddc142e
AlexNi245 <alex.plutta@googlemail.com>
2024-01-27 17:57:52 +0100

    fix modulo

--- test/Blake2s.test.ts
+++ test/Blake2s.test.ts
@@ -3,7 +3,7 @@ import { expect } from "chai";
 import crypto from 'crypto';
 import { Contract, JsonRpcProvider, toUtf8Bytes } from "ethers";
 import { ethers } from "hardhat";
-import { Blake2s } from "../typechain-types";
+import BLAKE2s from 'blake2s';
 
 describe("Blake2s", function () {
     let blake2s: Blake2s;
@@ -16,11 +16,27 @@ describe("Blake2s", function () {
     });
 
 
-    it.only("small digest", async () => {
+    it("small digest", async () => {
         const input = "hello world";
 
         const expected = crypto.createHash('blake2s256').update(input).digest();
-        const actual = await blake2s.blake2sFormatted(toUtf8Bytes(input), toUtf8Bytes(''), 32)
+        const actual = await blake2s.blake2sFormatted(toUtf8Bytes(input),)
+
+        expect(actual).to.equal("0x" + expected.toString('hex'))
+
+    })
+
+    it("large digest digest", async () => {
+        const right = "Contrary to popular belief  "
+        //   await blake2s.blake2sFormatted(toUtf8Bytes(right))
+        const wrong = " Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt, explicabo. Nemo enim ipsam voluptatem, quia voluptas sit, aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos, qui ratione voluptatem sequi nesciunt, neque porro quisquam est, qui dolorem ipsum, quia dolor sit amet consectetur adipisci[ng] velit, sed quia non numquam [do] eius modi tempora inci[di]dunt, ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum[d] exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? [D]Quis autem vel eum i[r]ure reprehenderit, qui in ea voluptate velit esse, quam nihil molestiae consequatur, vel illum, qui dolorem eum fugiat, quo voluptas nulla pariatur "
+        console.log("input length", wrong.length)
+        const expected = crypto.createHash('blake2s256').update(wrong).digest();
+        const actual = await blake2s.blake2sFormatted(toUtf8Bytes(wrong))
+
+
+
+
 
         expect(actual).to.equal("0x" + expected.toString('hex'))
 
